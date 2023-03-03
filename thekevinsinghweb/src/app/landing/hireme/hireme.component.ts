12c4ff4694f02cc151b01e39078b1183a85418f3
kevin singh <kevinsingh.developer@gmail.com>
2023-03-03 09:21:42 -0500

    Update packages

--- thekevinsinghweb/src/app/landing/hireme/hireme.component.ts
+++ thekevinsinghweb/src/app/landing/hireme/hireme.component.ts
@@ -1,9 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {FormBuilder, FormGroup, NgForm, Validators} from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthGService } from 'src/app/guard/auth-g.service';
 import { LandingService } from '../landing.service';
 import {ToastService} from '../../global_services/toast.service';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+
+
+
+interface Ticket {
+  name: string;
+  email: string;
+  message: string;
+}
 
 @Component({
   selector: 'hireme',
@@ -12,17 +21,25 @@ import {ToastService} from '../../global_services/toast.service';
 })
 export class HireMeComponent implements OnInit {
 
+  contactForm: FormGroup;
 
   constructor(
       private formBuilder: FormBuilder,
       private router: Router,
       private fetch: LandingService,
       private auth: AuthGService,
-      private toastService: ToastService
+      private toastService: ToastService,
+      private http: HttpClient
   ) { }
 
   ngOnInit(): void {
-
+    this.contactForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required]
+    });
   }
+
+
 
 }
