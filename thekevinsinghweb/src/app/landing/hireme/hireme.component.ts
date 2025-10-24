62ba05e928b570fc609a88917a8f34bcccc603d1
kevin singh <kevinsingh.developer@gmail.com>
2025-10-24 07:00:12 -0400

    Email JS deply

--- thekevinsinghweb/src/app/landing/hireme/hireme.component.ts
+++ thekevinsinghweb/src/app/landing/hireme/hireme.component.ts
@@ -6,6 +6,7 @@ import { LandingService } from '../landing.service';
 import {ToastService} from '../../global_services/toast.service';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import { $localize } from '@angular/localize/init'; // import $localize
+import emailjs from '@emailjs/browser';
 
 
 @Component({
@@ -41,15 +42,19 @@ export class HireMeComponent implements OnInit {
   onSubmit(form: NgForm) {
 
     if (form.valid) {
-      const name = form.value.name;
-      const email = form.value.email;
-      const message = form.value.message;
-      const currentDate = new Date().toISOString(); // Get the current date and time in ISO string format
+      const templateParams = {
+        from_name: form.value.name,
+        from_email: form.value.email,
+        message: form.value.message
+      };
 
-      // Add the currentDate to the form value
-      const formValue = {...form.value, currentDate};
-
-      this.landingService.saveContactMessage(formValue).subscribe(res => {
+      emailjs.send(
+        'service_gkrvp3p',      // Service ID
+        'template_1mcbyqw',     // Template ID
+        templateParams,
+        'Qek9RYRwkvcZ5aFFt'     // Public Key
+      ).then(
+        (response) => {
           this.toastService.show($localize`Thank you for your message!`, {
             classname: 'bg-success text-light',
             delay: 4000 ,
@@ -57,8 +62,17 @@ export class HireMeComponent implements OnInit {
             headertext: ''
           });
           form.reset();
+        },
+        (error) => {
+          this.toastService.show($localize`Failed to send message. Please try again.`, {
+            classname: 'bg-danger text-light',
+            delay: 4000 ,
+            autohide: true,
+            headertext: ''
+          });
+          console.error('EmailJS error:', error);
         }
-      )
+      );
     }
     else {
       this.toastService.show($localize`Invalid entry!`, {
